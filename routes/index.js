890b75a6a972c955d5fe35fe82bfbade8d8b6660
alu0100697414 <alu0100697414@ull.edu.es>
2015-11-09 19:48:20 +0000

    Creada vista con todas las preguntas del juego. Funcionando.

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,6 @@ router.get('/', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+router.get('/quizes/questions', quizController.questions);
+
 module.exports = router;
