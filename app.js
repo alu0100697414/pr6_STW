dbab0e97b7c8c001e8ec7b75e1eacf4240e37ca7
alu0100697414 <alu0100697414@ull.edu.es>
2015-11-09 11:49:59 +0000

    Añadido el favicon

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,9 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+// Genera el error 404 para el resto de rutas
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
